bdbc38891307cdb606138dedbcb7b7940e0c7e8a
danieljappe <danieljappe@icloud.com>
2022-09-26 12:15:01 +0200

    2. Daniel commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] Ags){
         System.out.println("hej gruppe, skiv en sout meddelelse \n pull, edit, commit, puch..");
+
+        System.out.println("hey hey, daniel her");
     }
 }
